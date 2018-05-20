fa2e95b140083f41ccb32361e5741477ce3611c9
u4bi <admin@naver.id>
2018-05-20 22:01:45 +0900

    Added uses to config.js.

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const express = require('express'),
-      bodyParser = require('body-parser'),
       config = require('./config');
 
 const api = require('./routes/api');
 
 const app = express();
 
-app.use('/static', express.static(__dirname + '/dist'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use('/', express.static(__dirname + '/dist'));
+for(v of config.uses) app.use(v);
+
 app.use(api);
-app.use((req, res, next) => [ res.header('Access-Control-Allow-Origin', '*'), res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'), next() ]);
 
 app.listen(config.port, () => console.log(`Example app listening on port ${ config.port }!`));
--- config/index.js
+++ config/index.js
@@ -1,5 +1,12 @@
+const bodyParser = require('body-parser');
+
 const config = {
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    uses : [
+        (req, res, next) => [res.header('Access-Control-Allow-Origin', '*'), res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'), next()],
+        bodyParser.json(),
+        bodyParser.urlencoded({ extended: true})
+    ]
 };
 
 module.exports = config;
