70ec4ffbd5ed01900dce78092e9269c3de266ab4
u4bi <admin@naver.id>
2018-05-20 21:35:31 +0900

    Added allow cors.

--- app.js
+++ app.js
@@ -10,5 +10,6 @@ app.use('/static', express.static(__dirname + '/dist'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(api);
+app.use((req, res, next) => [ res.header('Access-Control-Allow-Origin', '*'), res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'), next() ]);
 
 app.listen(config.port, () => console.log(`Example app listening on port ${ config.port }!`));
