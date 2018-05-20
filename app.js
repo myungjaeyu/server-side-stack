c147546f0b9dde9894935c5ad84f1ad15de0a3f8
u4bi <admin@naver.id>
2018-05-20 21:33:54 +0900

    Added body parser.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express'),
+      bodyParser = require('body-parser'),
       config = require('./config');
 
 const app = express();
 
 app.use('/static', express.static(__dirname + '/dist'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.listen(config.port, () => console.log(`Example app listening on port ${ config.port }!`));
