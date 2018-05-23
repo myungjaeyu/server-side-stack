8f5a7217613620318b1eb88edfedc58c30c9b432
u4bi <admin@naver.id>
2018-05-24 01:59:27 +0900

    Added auth with token example.

--- passport/api/auth.js
+++ passport/api/auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
     passport.authenticate('local', {session: false}, (err, user, info) => {
         
         return user ? 
-                    req.login(user, { session: false }, (err) => res.json( { info, user, token : jwt.sign(user, jwt_secret)})) :
+                    req.login(user, { session: false }, (err) => res.json( { info, token : jwt.sign(user, jwt_secret)})) :
                     res.json({ message: 'Something is not right', user   : user });
 
     })
