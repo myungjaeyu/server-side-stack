7b301b75d48f27a50d57ee3777935958a4f05e6e
u4bi <admin@naver.id>
2018-05-24 02:18:31 +0900

    code cleanup in header authorization.

--- passport/api/user.js
+++ passport/api/user.js
@@ -2,6 +2,9 @@ const passport = require('passport');
 
 module.exports = (req, res) => {
 
+    let token = req.query.access_token;
+    token && (req.headers.authorization = `Bearer ${ token }`);
+
     passport.authenticate('jwt', {session: false}, (err, user, info) => {
 
         return res.json({ info , user });
